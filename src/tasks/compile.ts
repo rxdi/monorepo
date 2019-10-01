f51e096b73229900e96e590dfe80c4350396e00c
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-10-01 14:01:00 +0300

    0.0.17

--- src/tasks/compile.ts
+++ src/tasks/compile.ts
@@ -1,10 +1,7 @@
 import { compileWorkspace } from '../helpers/compile-workspace';
 import { copyNodeModules } from '../helpers/copy-modules';
-import { includes } from '../helpers/args-extrators';
 
 export async function Compile() {
   await compileWorkspace();
-  if (includes('--copy-modules')) {
-    await copyNodeModules();
-  }
+  await copyNodeModules();
 }
